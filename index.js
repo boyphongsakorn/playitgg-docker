ff9ed3c43a93737389aa93bfe515f773931801cc
Phongsakorn Wisetthon <boy1556@hotmail.com>
2021-12-06 10:56:33 +0700

    test

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PlayIt = require('playit.gg');
   const playit = await PlayIt();
 
   // Create A PlayIt Tunnel
-  const tunnel = await playit.createTunnel({ proto: 'tcp', port: 9090, ip: '192.168.31.210' });
+  const tunnel = await playit.createTunnel({ proto: 'tcp', port: 80, ip: '192.168.31.210' });
 
   console.log(`http://${tunnel.url}`); // Print the tunnel url
 })();
